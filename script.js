03ea9dfb25cf1fe97a51560b7e5d4e48d16b099c
lee brian <leesinshingbrian@gmail.com>
2017-06-26 13:24:30 +0800

    added restart

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 document.getElementById('startButton').addEventListener('click', getNum)
 
+var start = document.getElementById('startButton')
+var restart = document.getElementById('restartButton')
 var h1 = document.querySelector('h1')
-var randomNumber = randomFn(0, 100)
-var inputNum;
+var randomNumber = randomFn(0, 10)
+var inputNum
 
 function getNum (n) {
   n = prompt('please key in a number')
@@ -15,12 +17,19 @@ function getNum (n) {
     // n = prompt("your guess is too small, try something bigger")
   } else if (inputNum === randomNumber) {
     h1.style.color = 'green'
-    confirm("that's right! let's try again!") //how to refresh?
+    confirm("that's right! let's try again!") // how to refresh?
+    restart.style.visibility = 'visible'
+    start.style.visibility = 'hidden'
+    restart.addEventListener('click', playAgain)
   }
 } // anyway to return a number and a function?
 console.log(randomNumber)
 function randomFn (min, max) {
   return Math.floor(Math.random() * (max - min)) + min
+}
+
+function playAgain () {
+  location.reload()
 }
 // problem here is that we are comparing undefined(inputNum) with number(randomNumber). How do we make inputNum into a number?
  // function isAnswerCorrect (inputNum, randomNumber) {
